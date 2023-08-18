38c9cebe732f6e34c172e6963e93ee903b8fe2a2
dojt <dotheis.work@gmail.com>
2023-08-18 18:01:54 +0200

    Version bump ➜ v0.2.0

--- src/DOT_StatsHelp.jl
+++ src/DOT_StatsHelp.jl
@@ -31,116 +31,391 @@
 ###########################################################################
 # File headers:1 ends here
 
-# [[file:../DOT_StatsHelp.org::*Module definition & imports][Module definition & imports:1]]
+# [[file:../DOT_StatsHelp.org::*Module definition, import, and recurrent exports][Module definition, import, and recurrent exports:1]]
 module DOT_StatsHelp
-# Module definition & imports:1 ends here
+# Module definition, import, and recurrent exports:1 ends here
 
-# [[file:../DOT_StatsHelp.org::*Module definition & imports][Module definition & imports:2]]
+# [[file:../DOT_StatsHelp.org::*Module definition, import, and recurrent exports][Module definition, import, and recurrent exports:2]]
 using DOT_NiceMath            # `⋅` = `*`  etc
 using DOT_NiceMath.Numbers64  # ℝ, ℤ, ℚ
 
-using LinearAlgebra: norm2, norm1, normInf as norm∞, norm_sqr as norm²
-# Module definition & imports:2 ends here
+using LinearAlgebra: norm2, norm1, normInf as norm∞, norm_sqr as norm2²,
+                     axpy!, axpby!
+# Module definition, import, and recurrent exports:2 ends here
 
-# [[file:../DOT_StatsHelp.org::*Basic types][Basic types:1]]
-export Stats
-# Basic types:1 ends here
+# [[file:../DOT_StatsHelp.org::*Module definition, import, and recurrent exports][Module definition, import, and recurrent exports:3]]
+export start_run!
+export record_step!
+export finalize_run!
+# Module definition, import, and recurrent exports:3 ends here
 
-# [[file:../DOT_StatsHelp.org::*The ~Stats~-type with constructor][The ~Stats~-type with constructor:1]]
-struct Stats{𝐑,V}              # `V` stands for valency of the tensor
-    # Input
-    true_μ        ::Array{ℝ, V}
+# [[file:../DOT_StatsHelp.org::*The mean process type: ~MeanProc{𝐑,V}~][The mean process type: ~MeanProc{𝐑,V}~:1]]
+export MeanProc
+# The mean process type: ~MeanProc{𝐑,V}~:1 ends here
 
-    # Output
-    empirical_μ   ::Array{𝐑, V}
+# [[file:../DOT_StatsHelp.org::*The mean process type: ~MeanProc{𝐑,V}~][The mean process type: ~MeanProc{𝐑,V}~:2]]
+struct MeanProc{𝐑 <: Real, V}              # `V` is an integer: the valency of the tensor
+# The mean process type: ~MeanProc{𝐑,V}~:2 ends here
 
-    err2²         ::Vector{ℝ}  # 2-norm of tensor; \
-    err1          ::Vector{ℝ}  # 1-norm  ~          | ultimate lengths:
-    err∞          ::Vector{ℝ}  # ∞-norm  ~         /   `outer_reps`
-    empirical_var ::Ref{𝐑}
+# [[file:../DOT_StatsHelp.org::*Fields and inner constructor][Fields and inner constructor:1]]
+#            Input for run
+    curr_true_μ  ::Array{ℝ, V} #                      size: dimension
 
-    # Transient data
-    _ws           ::Array{𝐑,V}
-end
-# The ~Stats~-type with constructor:1 ends here
+    #            Output of run
+    curr_emp_μ   ::Array{𝐑, V} #                      size: dimension
 
-# [[file:../DOT_StatsHelp.org::*The ~Stats~-type with constructor][The ~Stats~-type with constructor:2]]
-function Stats(true_μ ::Array{ℝ,V}, ::Type{𝐑}
-               ;
-               iterations_hint :: Int          )::Stats{𝐑,V}   where{𝐑<:Real,V}
+    #            Overall output
+    err2²        ::Array{ℝ,2}  # 2-norm of tensor; \
+    err1         ::Array{ℝ,2}  # 1-norm  ~          | size: `steps` ✕ `runs`
+    err∞         ::Array{ℝ,2}  # ∞-norm  ~         /
+    emp_var      ::Vector{𝐑}   #                      size: `runs`
 
-    empirical_μ   = Array{𝐑,V}(undef, size(true_μ) )  ; empirical_μ .= 𝐑(0)
-    _ws           = Array{𝐑,V}(undef, size(true_μ) )
+    #             Work space
+    ␣ws          ::Array{𝐑,V}  #                      size: dimension
 
-    err2² = sizehint!(ℝ[], iterations_hint)
-    err1  = sizehint!(ℝ[], iterations_hint)
-    err∞  = sizehint!(ℝ[], iterations_hint)
-    empirical_var = Ref{𝐑}(0.0)
+    #             Counters
+    𝐫            ::Ref{Int}    # index of current run (i.e., 0 ⪮ before first run)
+    𝐬            ::Ref{Int}    # index of current step (i.e., 0 ⪮ before first step)
 
-    return Stats{𝐑,V}(true_μ,
-                      empirical_μ, err2², err1, err∞, empirical_var,
-                      _ws)
-end
-# The ~Stats~-type with constructor:2 ends here
-
-# [[file:../DOT_StatsHelp.org::*Adding a new data point][Adding a new data point:1]]
-import Base: append!
-function append!(s ::Stats{𝐑,V}
-                 ;
-                 𝐸 ::Array{ℝ,V} ) ::Nothing  where{𝐑,V}
-
-    (;true_μ, err2², err1, err∞, empirical_μ, empirical_var, _ws) = s
-
-    @assert length(err2²) == length(err1) ==
-            length(err∞)
-
-    n = length(err2²)
-
-    let new_μ        = _ws
-        new_μ       .= n⋅empirical_μ/(n+1) .+ 𝐸/(n+1)
-        empirical_μ .= new_μ
+    #
+    # Convenience constructor -- not for the user
+    #
+    function
+    MeanProc{𝐑,V}(;
+               curr_true_μ ::Array{ℝ,V}, curr_emp_μ ::Array{𝐑,V}, emp_var ::Vector{𝐑},
+               err2² ::Array{ℝ,2}, err1 ::Array{ℝ,2}, err∞ ::Array{ℝ,2}, ␣ws ::Array{𝐑,V}) where{𝐑,V}
+        new(curr_true_μ, curr_emp_μ, err2², err1, err∞, emp_var, ␣ws,
+            0,0)
     end
+end
+# Fields and inner constructor:1 ends here
 
-    #
-    # Updating `mean…err`
-    #
-    let err  = _ws
-        err .= true_μ - empirical_μ
+# [[file:../DOT_StatsHelp.org::*Usage][Usage:1]]
+export err2², err1, err∞, emp_var, curr_emp_μ
+# Usage:1 ends here
 
-        push!( err2², norm²(err) )
-        push!( err1,  norm1(err) )
-        push!( err∞,  norm∞(err) )
+# [[file:../DOT_StatsHelp.org::*Usage][Usage:2]]
+err2²(  s ::MeanProc{𝐑,V}; run ::Int, step ::Int) where{𝐑,V} = ( @assert (1,1)≤(run,step)≤(s.𝐫[],s.𝐬[]); s.err2²[step,run] )
+err1(   s ::MeanProc{𝐑,V}; run ::Int, step ::Int) where{𝐑,V} = ( @assert (1,1)≤(run,step)≤(s.𝐫[],s.𝐬[]); s.err1[ step,run] )
+err∞(   s ::MeanProc{𝐑,V}; run ::Int, step ::Int) where{𝐑,V} = ( @assert (1,1)≤(run,step)≤(s.𝐫[],s.𝐬[]); s.err∞[ step,run] )
+emp_var(s ::MeanProc{𝐑,V}; run ::Int)             where{𝐑,V} = ( @assert 1    ≤run ≤ s.𝐫[]             ; s.emp_var[run]    )
+
+curr_emp_μ(s ::MeanProc{𝐑,V})                     where{𝐑,V} = ( @assert 1 ≤ s.𝐫[]                     ; s.curr_emp_μ      )
+# Usage:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*User-facing constructor for ~MeanProc~ <<mp-constructor>>][User-facing constructor for ~MeanProc~  <<mp-constructor>>:1]]
+function MeanProc(dimension ::NTuple{V,Int}
+                  ;
+                  steps :: Int,
+                  runs  :: Int,
+                  𝐑     :: Type{<:Real} = ℝ)  ::MeanProc     where{V}
+# User-facing constructor for ~MeanProc~  <<mp-constructor>>:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*User-facing constructor for ~MeanProc~ <<mp-constructor>>][User-facing constructor for ~MeanProc~  <<mp-constructor>>:2]]
+curr_true_μ   = Array{ℝ,V}(undef, dimension )
+    curr_emp_μ    = Array{𝐑,V}(undef, dimension )   ; curr_emp_μ   .= 𝐑(0)
+    ␣ws           = Array{𝐑,V}(undef, dimension )
+
+    err2²         = Array{ℝ,2}(undef, steps,runs)
+    err1          = Array{ℝ,2}(undef, steps,runs)
+    err∞          = Array{ℝ,2}(undef, steps,runs)
+    emp_var       = Array{𝐑,1}(undef, runs)         ; emp_var .= 𝐑(0)
+
+    s = MeanProc{𝐑,V}( ; curr_true_μ, curr_emp_μ,
+                         err2², err1, err∞, emp_var,  ␣ws)
+    ␣integrity_check(s)
+    return s
+end
+# User-facing constructor for ~MeanProc~  <<mp-constructor>>:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*Helper functions and integrity check][Helper functions and integrity check:1]]
+valency(        s ::MeanProc{𝐑,V} ) where{𝐑,V}    = V
+dimension(      s ::MeanProc{𝐑,V} ) where{𝐑,V}    = size( s.curr_true_μ )
+numo_stepsruns( s ::MeanProc{𝐑,V} ) where{𝐑,V}    = size( s.err2²       )
+numo_steps(     s ::MeanProc{𝐑,V} ) where{𝐑,V}    = numo_stepsruns(s) |> first
+numo_runs(      s ::MeanProc{𝐑,V} ) where{𝐑,V}    = numo_stepsruns(s) |> last
+# Helper functions and integrity check:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Helper functions and integrity check][Helper functions and integrity check:2]]
+function ␣integrity_check(s ::MeanProc{𝐑,V}) ::Nothing  where{𝐑,V}
+# Helper functions and integrity check:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*Implementation][Implementation:1]]
+@assert size( s.curr_true_μ ) == dimension(s) == size( s.curr_emp_μ  )
+
+    let steps  = numo_steps(s),
+        runs   = numo_runs(s),
+        dim    = dimension(s)
+
+        @assert steps > 1
+        @assert runs  ≥ 1
+
+        @assert 0 ≤ s.𝐫[] ≤ runs
+        @assert 0 ≤ s.𝐬[] ≤ steps
+        @assert s.𝐫[] ≥ 1 || s.𝐬[] == 0
+
+        @assert size(     s.err2²       ) == (steps,runs)
+        @assert size(     s.err1        ) == (steps,runs)
+        @assert size(     s.err∞        ) == (steps,runs)
+        @assert size(     s.emp_var     ) == (runs,)
+
+        @assert size(     s.␣ws         ) == dim
     end #^ let
+    return nothing
+end
+# Implementation:1 ends here
 
-    #
-    # Updating variance
-    #
-    # We record the simple biased estimate of the empirical variance, and
-    # correct it in the `finalize()` function.
+# [[file:../DOT_StatsHelp.org::*Starting a new run: ~start_run!()~ <<mp-start>>][Starting a new run: ~start_run!()~ <<mp-start>>:1]]
+function start_run!(s      :: MeanProc{𝐑,V}
+                    ;
+                    true_μ :: Array{ℝ,V} ) ::Nothing  where{𝐑,V}
+# Starting a new run: ~start_run!()~ <<mp-start>>:1 ends here
 
-    empirical_var[] = n⋅empirical_var[]/(n+1) + norm²( 𝐸 - empirical_μ )/(n+1)
+# [[file:../DOT_StatsHelp.org::*Implementation of ~start_run!()~][Implementation of ~start_run!()~:1]]
+␣integrity_check(s)
 
+
+    if    s.𝐫[] > 0         @assert s.𝐬[] == numo_steps(s)
+    else                    @assert s.𝐬[] == 0               end
+
+    s.𝐫[] += 1            ; @assert s.𝐫[] ≤ numo_runs(s)
+    s.𝐬[]  = 0
+
+    @assert size(true_μ) == dimension(s)
+
+    let 𝐫 = s.𝐫[],
+        𝐬 = s.𝐬[]
+
+        s.curr_true_μ .= true_μ
+        s.curr_emp_μ  .= 𝐑(0)
+        s.emp_var[𝐫]   = 𝐑(0)
+
+    end
     nothing;
-end #^ append!()
-# Adding a new data point:1 ends here
+end #^ start_run!()
+# Implementation of ~start_run!()~:1 ends here
 
-# [[file:../DOT_StatsHelp.org::*Finalizing stats collection][Finalizing stats collection:1]]
-function finalize!(s ::Stats{𝐑,V}) ::Nothing                  where{𝐑,V}
+# [[file:../DOT_StatsHelp.org::*Adding data of a step: ~record_step!()~ <<mp-record>>][Adding data of a step: ~record_step!()~ <<mp-record>>:1]]
+function record_step!(s ::MeanProc{𝐑,V}
+                      ;
+                      𝐸 ::Array{ℝ,V} ) ::Nothing  where{𝐑,V}
+# Adding data of a step: ~record_step!()~ <<mp-record>>:1 ends here
 
-    (;err2², err1, err∞, empirical_var) = s
+# [[file:../DOT_StatsHelp.org::*Implementation][Implementation:1]]
+␣integrity_check(s)
 
-    @assert length(err2²) ==
-            length(err1)  == length(err∞)
+(;curr_true_μ, curr_emp_μ, err2², err1, err∞, emp_var, ␣ws) = s
+
+
+s.𝐬[] += 1            ; @assert s.𝐬[] ≤ numo_steps(s)
+
+let 𝐫     = s.𝐫[],
+    𝐬     = s.𝐬[],
+    steps = numo_steps(s)
+
+    #
+    # Note order between emp var and emp μ
+    #
+    # emp_var[𝐫]   = (𝐬-1) ⋅ emp_var[𝐫]  / 𝐬    +   (𝐬-1) ⋅ norm2²( curr_emp_μ - 𝐸 ) / 𝐬²
+    # curr_emp_μ  .= (𝐬-1) ⋅ curr_emp_μ / 𝐬   +   𝐸 / 𝐬
+
+    ␣ws         .= curr_emp_μ
+    axpby!(-1/𝐬, 𝐸,  1/𝐬, ␣ws)
+    emp_var[𝐫]   = (𝐬-1) ⋅ (   emp_var[𝐫]  / 𝐬    +   norm2²( ␣ws )   )
+                  # will be corrected for bias in finalize_run!()
+
+    axpby!( 1/𝐬, 𝐸, (𝐬-1)/𝐬, curr_emp_μ)
+
+    #
+    # Errors
+    #
+    ␣ws         .= curr_emp_μ - curr_true_μ
+
+    err2²[𝐬,𝐫]   = norm2²(␣ws)
+    err1[ 𝐬,𝐫]   = norm1(␣ws)
+    err∞[ 𝐬,𝐫]   = norm∞(␣ws)
+end #^ let
+nothing;
+end #^ record_step!()
+# Implementation:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Finalizing a run: ~finalize_run!()~ <<mp-finalize>>][Finalizing a run: ~finalize_run!()~ <<mp-finalize>>:1]]
+function finalize_run!(s ::MeanProc{𝐑,V}) ::Nothing                  where{𝐑,V}
+# Finalizing a run: ~finalize_run!()~ <<mp-finalize>>:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Implementation][Implementation:1]]
+␣integrity_check(s)
+
+    @assert s.𝐬[] == numo_steps(s)
 
     #
     # Un-bias empirical variance:
     #
-    let n=length(err2²)
-        empirical_var[] *= (n-1)/n
+    let 𝐫     = s.𝐫[],
+        𝐬     = s.𝐬[]
+
+        s.emp_var[ 𝐫 ] *= 𝐬 / 𝐑(𝐬-1)
     end
     nothing;
-end #^ finalize!(::Stats)
-# Finalizing stats collection:1 ends here
+end #^ finalize_run!()
+# Implementation:1 ends here
+
+# [[file:../DOT_StatsHelp.org::␣xtiles_make()][␣xtiles_make()]]
+function ␣xtiles_make(_𝝅) ::Tuple
+    𝝅 = collect(_𝝅)
+    sort!(𝝅)
+
+    @assert allunique( 𝝅 )
+    @assert 0.0 < 𝝅[1] ≤ 𝝅[end] == 1.0
+
+    L = length(𝝅) # just saying...
+    return (𝝅...,)
+end
+# ␣xtiles_make() ends here
+
+# [[file:../DOT_StatsHelp.org::␣xtiles_count!()][␣xtiles_count!()]]
+function ␣xtiles_count!(freqs ::AbstractArray{ℝ},
+                        𝝅     ::NTuple{L,ℝ}
+                        ;
+                        p     ::ℝ,
+                        Δ     ::ℝ                 )::NamedTuple    where{L}
+    @assert 0-1e-50 ≤ p
+    @assert           p ≤ 1+1e-30
+    @assert L == length(freqs)
+
+    ℓ = 1
+    while ℓ ≤ L   &&   𝝅[ℓ] < p
+        ℓ += 1
+    end
+    ℓ = min(ℓ,L)                  # in case of rounding errors near 1.0
+
+    freqs[ ℓ ] += Δ
+
+    return ( ℓ=ℓ,  lo=get(𝝅,ℓ-1,0.0), hi=𝝅[ℓ] )
+end
+# ␣xtiles_count!() ends here
+
+# [[file:../DOT_StatsHelp.org::*The max-approx process type: ~MaxProc{L}~][The max-approx process type: ~MaxProc{L}~:1]]
+export MaxProc
+# The max-approx process type: ~MaxProc{L}~:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*The max-approx process type: ~MaxProc{L}~][The max-approx process type: ~MaxProc{L}~:2]]
+mutable struct MaxProc{L}
+    # consts
+    const 𝝅         ::NTuple{L,ℝ}   # percentile numbers, sorted increasingly (last one must be 1.0)
+    const freqs     ::Array{ℝ,2}    #
+    const steps     ::Int
+    const runs      ::Int
+
+    # mutables
+    true_max  ::ℝ
+    curr_max  ::ℝ
+    𝐫         ::Int    # index of current run (i.e., 0 ⪮ before first run)
+    𝐬         ::Int    # index of current step (i.e., 0 ⪮ before first step)
+end
+# The max-approx process type: ~MaxProc{L}~:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*User-facing constructor for ~MaxApprox~ <<max-constructor>>][User-facing constructor for ~MaxApprox~ <<max-constructor>>:1]]
+function MaxProc(_𝝅
+                 ;
+                 steps :: Int,
+                 runs  :: Int )  ::MaxProc
+# User-facing constructor for ~MaxApprox~ <<max-constructor>>:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Implementation][Implementation:1]]
+𝝅     = ␣xtiles_make(_𝝅)
+    L     = length(𝝅)
+    freqs = Array{ℝ,2}(undef,steps,L)
+    s     = MaxProc{L}(𝝅, freqs, steps, runs, Inf, Inf, 0,0)
+    ␣integrity_check(s)
+    return s
+end
+# Implementation:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Helper functions and integrity check][Helper functions and integrity check:1]]
+numo_stepsruns( s ::MaxProc{L} ) where{L}    = ( numo_steps(s) , numo_runs(s) )
+numo_steps(     s ::MaxProc{L} ) where{L}    = s.steps
+numo_runs(      s ::MaxProc{L} ) where{L}    = s.runs
+# Helper functions and integrity check:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Helper functions and integrity check][Helper functions and integrity check:2]]
+function ␣integrity_check(s ::MaxProc{L}) ::Nothing where{L}
+    @assert L           == length(s.𝝅)  "Crazy bug!!"
+    @assert 0 < s.𝝅[1] < s.𝝅[end] == 1.0
+    @assert (L,s.steps) == size(s.freqs)
+    @assert s.steps     == numo_steps(s)
+    @assert s.runs      == numo_runs(s)
+    @assert s.steps ≥ 1
+    @assert s.runs  ≥ 1
+
+    @assert 0 ≤ s.𝐬 ≤ s.steps
+    @assert 0 ≤ s.𝐫 ≤ s.runs
+    @assert s.𝐫 ≥ 1 || s.𝐬 == 0
+
+    @assert s.curr_max ≤ s.true_max
+
+    nothing;
+end
+# Helper functions and integrity check:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*Starting a new run: ~start_run!()~ <<max-start>>][Starting a new run: ~start_run!()~ <<max-start>>:1]]
+function start_run!(s        :: MaxProc{L}
+                    ;
+                    true_max :: ℝ            ) ::Nothing  where{L}
+    ␣integrity_check(s)
+
+    if    s.𝐫 > 0           @assert s.𝐬 == numo_steps(s)
+    else                    @assert s.𝐬 == 0               end
+
+    s.𝐫 += 1              ; @assert s.𝐫 ≤ numo_runs(s)
+    s.𝐬  = 0
+
+    s.true_max = true_max
+    s.curr_max = -Inf
+
+    nothing;
+end
+# Starting a new run: ~start_run!()~ <<max-start>>:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Adding data of a step: ~record_step!()~ <<max-record>>][Adding data of a step: ~record_step!()~ <<max-record>>:1]]
+function record_step!(s ::MaxProc{L}
+                      ;
+                      𝐸 ::ℝ            ) ::Union{ℝ,Nothing}     where{L}
+    ␣integrity_check(s)
+
+    @assert 0 ≤ 𝐸 ≤ s.true_max
+
+    (;runs,true_max,𝝅,freqs) = s
+
+    new_max ::Bool = false
+    if 𝐸 > s.curr_max
+        s.curr_max = 𝐸
+        new_max    = true
+    end
+
+    s.𝐬        += 1            ; @assert s.𝐬 ≤ numo_steps(s)
+    freqsₛ    =  @view freqs[:,s.𝐬]
+    (;ℓ,lo,hi) =  ␣xtiles_count!(freqsₛ, 𝝅
+                                ; p = s.curr_max / true_max,  Δ=1/runs)
+
+    if new_max
+        @info "New max: $𝐸; ℓ=$ℓ, lo=$lo, hi=$hi"
+        return lo
+    end
+    nothing;
+end #^ record_step!()
+# Adding data of a step: ~record_step!()~ <<max-record>>:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Finalizing a run: ~finalize_run!()~ <<max-finalize>>][Finalizing a run: ~finalize_run!()~ <<max-finalize>>:1]]
+function finalize_run!(s ::MaxProc{L}) ::Nothing         where{L}
+    ␣integrity_check(s)
+
+    @assert s.𝐬 == numo_steps(s)
+    @assert 0 ≤ s.curr_max
+
+    nothing; # ... else needs to be done
+end #^ finalize_run!()
+# Finalizing a run: ~finalize_run!()~ <<max-finalize>>:1 ends here
 
 # [[file:../DOT_StatsHelp.org::*End of module][End of module:1]]
 end #^ module SPSA_Shift
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,16 +6,238 @@
 #  (The actual source code is in the Org file.)                           #
 #                                                                         #
 ###########################################################################
+# File headers:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*Importing things][Importing things:1]]
+using DOT_StatsHelp
 
 using Test
-using DOT_StatsHelp
-# File headers:2 ends here
+
+
+using DoubleFloats: Double64
+
+using LinearAlgebra: norm_sqr as norm2², norm2, norm1, normInf as norm∞
+
+using Statistics: mean, var
+
+using DOT_NiceMath
+using DOT_StatsHelp.Numbers64     # === DOT_NiceMath.Numbers64 -- just making sure we get the same!
+# Importing things:1 ends here
 
 # [[file:../DOT_StatsHelp.org::*Generic test based on ~JET.jl~][Generic test based on ~JET.jl~:1]]
 using JET
-using JSON # Only for ignoring by JET
+using JSON        # Only for ignoring by JET
+using Polynomials # Only for ignoring by JET
 
 @testset verbose=true "DOT_StatsHelp.jl testing:  via JET.jl" begin
-    test_package(DOT_StatsHelp, ignored_modules=(AnyFrameModule(JSON.Parser),) )
+    test_package(DOT_StatsHelp, ignored_modules=(AnyFrameModule(JSON.Parser),AnyFrameModule(Polynomials),) )
 end
 # Generic test based on ~JET.jl~:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Set up testset][Set up testset:1]]
+@testset verbose=true "DOT_StatsHelp.jl testing: Test MeanProc{}" begin
+# Set up testset:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Test with valency 0][Test with valency 0:1]]
+function test__meanestim_0(;runs=1:10,steps=2:4:20)
+    for (curr_runs,curr_steps) in Iterators.product(runs,steps)
+
+        data = 100*randn(curr_steps,curr_runs)
+
+        mp = MeanProc( () ; steps=curr_steps, runs=curr_runs, 𝐑=Double64)
+
+        for run = 1:curr_runs
+
+            start_run!(mp ; true_μ = fill(0.0) )
+
+            for step = 1:curr_steps
+                record_step!(mp ; 𝐸 = fill(data[step,run]) )
+                @test curr_emp_μ(mp)[]  ≈ mean( @view data[1:step,run] )
+            end
+            finalize_run!(mp)
+
+            @test emp_var(mp;run)         ≈ var(  @view data[:,run] )
+
+            for step=1:curr_steps
+                @test  err2²(mp;run,step) ≈ mean( data[1:step,run] ) |> abs²
+            end
+            @test all(
+                err1(mp;run,step)         ≈ mean( data[1:step,run] ) |> abs
+                for step=1:curr_steps
+            )
+            @test all(
+                err∞(mp;run,step)         ≈ mean( data[1:step,run] ) |> abs
+                for step=1:curr_steps
+            )
+
+        end #^ for run
+
+    end #^ for curr_...
+end #^ test__meanestim_0()
+# Test with valency 0:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Test with valency 0][Test with valency 0:2]]
+@testset "Valency-0 tests" begin
+    test__meanestim_0()
+end
+# Test with valency 0:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*Test with valency 1][Test with valency 1:1]]
+function test__meanestim_1(;runs=1:3:9,steps=2:5:12)
+    for (curr_runs,curr_steps) in Iterators.product(runs,steps)
+
+        dim  = 31
+
+        data = [ randn(dim) for s=1:curr_steps, r=1:curr_runs ]
+
+        mp = MeanProc( (dim,) ; steps=curr_steps, runs=curr_runs, 𝐑=Double64)
+
+        for run = 1:curr_runs
+
+            start_run!(mp ; true_μ = zeros(31) )
+
+            for step = 1:curr_steps
+                record_step!(mp ; 𝐸 = data[step,run] )
+                @test curr_emp_μ(mp)  ≈ mean( @view data[1:step,run] )
+            end
+            finalize_run!(mp)
+
+            @test emp_var(mp;run)         ≈ var( @view data[:,run] ) |> norm1 # Julia `var` returns array
+
+            for step=1:curr_steps
+                @test  err2²(mp;run,step) ≈ mean( data[1:step,run] ) |> norm2²
+            end
+            @test all(
+                err1(mp;run,step)         ≈ mean( data[1:step,run] ) |> norm1
+                for step=1:curr_steps
+            )
+            @test all(
+                err∞(mp;run,step)         ≈ mean( data[1:step,run] ) |> norm∞
+                for step=1:curr_steps
+            )
+
+        end #^ for run
+
+    end #^ for curr_...
+end #^ test__meanestim_1()
+# Test with valency 1:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Test with valency 1][Test with valency 1:2]]
+@testset "Valency-1 tests" begin
+    test__meanestim_1()
+end
+# Test with valency 1:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*Test with valency 2][Test with valency 2:1]]
+function test__meanestim_2(;runs=1:3:9,steps=2:5:12)
+    for (curr_runs,curr_steps) in Iterators.product(runs,steps)
+
+        sz  = (7,13)
+
+        data = [ randn(sz) for s=1:curr_steps, r=1:curr_runs ]
+
+        mp = MeanProc( (sz) ; steps=curr_steps, runs=curr_runs, 𝐑=Double64)
+
+        for run = 1:curr_runs
+
+            start_run!(mp ; true_μ = zeros(sz) )
+
+            for step = 1:curr_steps
+                record_step!(mp ; 𝐸 = data[step,run] )
+                @test curr_emp_μ(mp)  ≈ mean( @view data[1:step,run] )
+            end
+            finalize_run!(mp)
+
+            @test emp_var(mp;run)         ≈ var( @view data[:,run] ) |> norm1 # Julia `var` returns array
+
+            for step=1:curr_steps
+                @test  err2²(mp;run,step) ≈ mean( data[1:step,run] ) |> norm2²
+            end
+            @test all(
+                err1(mp;run,step)         ≈ mean( data[1:step,run] ) |> norm1
+                for step=1:curr_steps
+            )
+            @test all(
+                err∞(mp;run,step)         ≈ mean( data[1:step,run] ) |> norm∞
+                for step=1:curr_steps
+            )
+
+        end #^ for run
+
+    end #^ for curr_...
+end #^ test__meanestim_1()
+# Test with valency 2:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Test with valency 2][Test with valency 2:2]]
+@testset "Valency-2 tests" begin
+    test__meanestim_2()
+end
+# Test with valency 2:2 ends here
+
+# [[file:../DOT_StatsHelp.org::*End of testset][End of testset:1]]
+end #^ testset
+# End of testset:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Main testing function][Main testing function:1]]
+function test__xtiles()
+
+    function some_tests__interior(_𝝅)
+        L     = length(_𝝅)
+        m     = 16
+        N     = m⋅L
+        𝝅     = DOT_StatsHelp.␣xtiles_make(_𝝅)
+        freqs = zeros(ℝ,L)
+
+        for ℓ = 1 : L
+            lo = get(𝝅,ℓ-1,   0.0)
+            hi =     𝝅[ℓ  ]
+            @test lo < hi || (lo==hi && ℓ==L)
+            for j = 1:m
+                p =  lo + (hi-lo)⋅rand()
+                iv = DOT_StatsHelp.␣xtiles_count!(freqs,𝝅 ; p, Δ=1/N)
+                @test iv.lo < p ≤ iv.hi
+            end
+        end
+
+        @test sum(freqs) ≈ 1
+        for ℓ = 1:L
+            @test freqs[ℓ] ≈ m/N
+        end
+    end
+
+    function some_tests__boundary(_𝝅)
+        L     = length(_𝝅)
+        m     = 16
+        N     = m⋅L
+        𝝅     = DOT_StatsHelp.␣xtiles_make(_𝝅)
+        freqs = zeros(ℝ,L)
+
+        for j = 1:m
+            for ℓ = 1:L
+                lo = get(𝝅,ℓ-1,   0.0)
+                hi =     𝝅[ℓ  ]
+                @test lo < hi || (lo==hi && ℓ==L)
+                DOT_StatsHelp.␣xtiles_count!(freqs,𝝅 ; p=hi, Δ=1/N)
+            end
+        end
+
+        @test sum(freqs) ≈ 1
+        for ℓ = 1:L
+            @test freqs[ℓ] ≈ m/N
+        end
+    end
+
+    for L = 1:10
+        𝝅 = [ rand(L-1)
+              1.0       ]
+        some_tests__interior(𝝅)
+        some_tests__boundary(𝝅)
+    end
+end #^ test__Xtiles()
+# Main testing function:1 ends here
+
+# [[file:../DOT_StatsHelp.org::*Call the testing function][Call the testing function:1]]
+@testset verbose=true "DOT_StatsHelp.jl testing: Test Xtiles helper" begin
+    test__xtiles()
+end
+# Call the testing function:1 ends here
